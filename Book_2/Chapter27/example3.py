79b9468b0d034f8c7f46aff3b5bb7f445dc5d981
OIITuMuCT <vataman@live.ru>
2025-02-03 21:37:05 +0300

    example3

---
+++ Book_2/Chapter27/example3.py
@@ -0,0 +1,35 @@
+import example2
+
+
+class ThirdClass(example2.SecondClass):
+    def __init__(self, value):
+        self.data = value
+
+    def __add__(self, ohter):
+        return ThirdClass(self.data + ohter)
+    
+    def plus(self, other):
+        return ThirdClass(self.data + other)
+
+    def __str__(self):
+        return f"[ThirdClass: {self.data}]"
+
+    def mul(self, other):
+        self.data *= other
+
+
+if __name__ == "__main__":
+    a = ThirdClass("abc")
+    a.display()
+    print(a + 'zya')
+    b = a + "xyz"
+    b.display()
+    c = a.plus('PYTHON')
+    print(isinstance(c, ThirdClass))
+    print(isinstance(c, str))
+    print(b)
+    a.mul(3)
+    a.display()
+    print(a)
+    print(isinstance(b, ThirdClass))
+
