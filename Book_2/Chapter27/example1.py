3d43df009d2b50db9b1d7bfd6136384998b67381
OIITuMuCT <vataman@live.ru>
2025-02-03 20:40:01 +0300

    FirstClass

---
+++ Book_2/Chapter27/example1.py
@@ -0,0 +1,16 @@
+class FirstClass:
+    def setdata(self, value):
+        self.data = value
+    def display(self):
+        print(self.data)
+
+
+if __name__ == '__main__':
+    first = FirstClass()
+    first.setdata("Potato")
+    first.display()
+    tomato = FirstClass()
+    tomato.setdata('tomato')
+    tomato.display()
+    tomato.data = "Cucumber"
+    tomato.display()
