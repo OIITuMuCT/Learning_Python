159a73ba775d0dad03c7031164a80897238bdf2b
OIITuMuCT <vataman@live.ru>
2025-02-04 21:37:26 +0300

    realistic_example add two methods

--- Book_2/Chapter27/realistic_example.py
+++ Book_2/Chapter27/realistic_example.py
@@ -3,6 +3,11 @@ class Person:
         self.name = name
         self.job = job
         self.pay = pay
+    def lastName(self):
+        return self.name.split()[-1]
+    def giveRaise(self, percent):
+        self.pay = int(self.pay * (1 + percent))
+
 
 
 class Manager(Person):
@@ -15,3 +20,8 @@ if __name__ == '__main__':
     
     print(bob.name, bob.pay)
     print(sue.name, sue.pay)
+    print(bob.name.split()[-1])
+    sue.pay *=1.10
+    print('%.2f' % sue.pay)
+    print(sue.pay)
+    
