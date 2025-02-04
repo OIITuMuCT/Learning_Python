7c07231408b13841087c14e812bf975774939d95
OIITuMuCT <vataman@live.ru>
2025-02-04 22:48:00 +0300

    person-composite

---
+++ Book_2/Chapter27/person-composite.py
@@ -0,0 +1,47 @@
+class Person:
+    def __init__(self, name, job=None, pay=0):
+        self.name = name
+        self.job = job
+        self.pay = pay
+
+    def lastName(self):
+        return self.name.split()[-1]
+
+    def giveRaise(self, percent):
+        self.pay = int(self.pay * (1 + percent))
+
+    def __repr__(self):
+        return f"[Person: {self.name}, {self.pay}]"
+
+
+class Manager:
+    def __init__(self, name, pay):
+        self.person = Person(name, 'mgr', pay)
+    def giveRaise(self, percent, bonus=0.10):  # Хороший способ:
+        self.person.giveRaise(percent + bonus)  # исходной версии
+    def __getattr__(self, attr):
+        return getattr(self.person, attr)
+    def __repr__(self):
+        return str(self.person)
+    
+if __name__=='__main__':
+    bob = Person('Bob Smith')
+    sue = Person('Sue Jones', job='dev', pay=100000)
+    print(bob.name, bob.pay)
+    print(sue.name, sue.pay)
+    print(bob.lastName(), sue.lastName())
+    sue.giveRaise(.10)
+    print(sue.pay)
+    print(sue.__dict__)
+    print(list(name for name in Person.__dict__.keys() if not name.endswith('__')))
+    print(bob)
+    print(sue)
+    # tom = Manager('Tom Jones', 'mgr', 50000)  # Создать экземпляр
+    tom = Manager('Tom Jones', 50000)
+    tom.giveRaise(.10)
+    print(tom.lastName())
+    print(tom)
+    print('--All three--')
+    for obj in (bob, sue, tom):
+        obj.giveRaise(.10)
+        print(obj)
