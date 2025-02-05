abdd78425285c335450cf581b2f3750d5536aa01
OIITuMuCT <vataman@live.ru>
2025-02-05 09:43:11 +0300

    classtools TopTest, SubTest

--- Book_2/Chapter27/class_tools.py
+++ Book_2/Chapter27/class_tools.py
@@ -12,7 +12,16 @@ class AttrDisplay:
         return ', '.join(attrs)
     def __repr__(self):
         return '[%s: %s]' % (self.__class__.__name__, self.getherAttrs())
-    
-    if __name__ == '__main__':
-        class TopTest(AttrDisplay):
-            pass
+
+if __name__ == '__main__':
+    class TopTest(AttrDisplay):
+        count = 0
+        def __init__(self):
+            self.attr1 = TopTest.count
+            self.attr2 = TopTest.count+1
+            TopTest.count += 2
+    class SubTest(TopTest):
+        pass
+    X , Y = TopTest(), SubTest()
+    print(X)
+    print(Y)
