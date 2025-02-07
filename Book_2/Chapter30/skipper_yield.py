171a9d440f092a061f967b15c61cf64f54b0424c
OIITuMuCT <vataman@live.ru>
2025-02-07 16:35:15 +0300

    contains

--- Book_2/Chapter30/skipper_yield.py
+++ Book_2/Chapter30/skipper_yield.py
@@ -1,11 +1,11 @@
-class SkipperObject:
-    def __init__(self, wrapped):
-        self.wrapped = wrapped
-    
+class SkipperObject:                    # Еще один генератор на основе__ iter__ + yield
+    def __init__(self, wrapped):        # Область видимости экземпляра сохраняется нормально
+        self.wrapped = wrapped          # Состояние локальной области видимости сохраняется
+                                        # автоматически
+
     def __iter__(self):
         offset = 0
         while offset < len(self.wrapped):
             item = self.wrapped[offset]
             offset += 2
             yield item
-    
