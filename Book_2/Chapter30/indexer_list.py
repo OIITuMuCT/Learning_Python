04fc162e695988b366c87716f315efe05e71f5d1
OIITuMuCT <vataman@live.ru>
2025-02-07 11:34:53 +0300

    indexer_list

--- Book_2/Chapter30/indexer_list.py
+++ Book_2/Chapter30/indexer_list.py
@@ -1,12 +1,15 @@
 class Indexer:
     data = [5,6,7,8,9]
     def __getitem__(self, index):
-        print('getitem: , index')
-        return self.data[index]
-    
+        if isinstance(index, int):
+            print('indexing',  index)
+        else:
+            print('slicing', index.start, index.stop, index.step)
+
 
 
 if __name__ == '__main__':
     X = Indexer()
-    print(X[0])
-    print(X[2:4])
+    print(X[99])
+    # print(X[0])
+    # print(X[2:4])
