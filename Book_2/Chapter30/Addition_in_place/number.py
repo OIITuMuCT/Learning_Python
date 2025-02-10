d81687aa17c37db15c6f31d092cd9a504ca195f2
OIITuMuCT <vataman@live.ru>
2025-02-10 15:18:02 +0300

    Addition in place

--- Book_2/Chapter30/Addition_in_place/number.py
+++ Book_2/Chapter30/Addition_in_place/number.py
@@ -4,3 +4,13 @@ class Number:
     def __iadd__(self, other):
         self.val += other
         return self
+
+if __name__ == "__main__":
+    x = Number(5)
+    x += 1
+    x += 1
+    print(x.val)
+    y = Number([1])
+    y += [2]
+    y += [3]
+    print(y.val)
