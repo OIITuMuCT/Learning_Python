d3794ab705e5691f6d945e6a6925c19b866b4fae
OIITuMuCT <vataman@live.ru>
2025-02-11 19:05:04 +0300

    Mixin ListInstance

---
+++ Book_2/Chapter31/multiple_inheritance/listinstance.py
@@ -0,0 +1,26 @@
+#!python
+
+class ListInstance:
+    """
+    Подмешиваемый класс, который представляет форматированную функцию
+    print() или str(); отображает только атрибуты экземпляра; self
+    является экземпляром самого нижнего класса;
+    имена __Х предотвращают конфликты с атрибутами клиента  
+    """
+    def __attrnames(self):
+        result = ''
+        for attr in sorted(self.__dict__):
+            result += '\t%s=%s\n' % (attr, self.__dict__[attr])
+        return result
+    # def __attrnames(self):
+    #     return ''.join('\t%s=%s\n' % (attr, self.__dict__[attr]))
+    #     for attr in sorted(self.__dict__)
+    def __str__(self):
+        return '<Instance of %s, address %s:\n%s>' % (
+            self.__class__.__name__,
+            id(self),
+            self.__attrnames()
+        )
+if __name__ == '__main__':
+    import testmixin
+    testmixin.tester(ListInstance)
---
+++ Book_2/Chapter31/multiple_inheritance/mixins.py
@@ -0,0 +1,7 @@
+class Spam:
+    def __init__(self):
+        self.data1 = 'food'
+
+if __name__ =='__main__':
+    X = Spam()
+    print(X)
