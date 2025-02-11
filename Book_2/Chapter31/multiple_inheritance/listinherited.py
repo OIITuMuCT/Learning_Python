48e39e77c45b3b82ec3113deb0ef6ee2f04559ab
OIITuMuCT <vataman@live.ru>
2025-02-11 22:13:30 +0300

    listinherited2.py

--- Book_2/Chapter31/multiple_inheritance/listinherited.py
+++ Book_2/Chapter31/multiple_inheritance/listinherited.py
@@ -18,6 +18,7 @@ class ListInherited:
             else:
                 result += '\t%s=%s\n' % (attr, getattr(self, attr))
         return result
+
     def __str__(self):
         return '<Instance of %s, address %s:\n%s>' % (
             self.__class__.__name__,
---
+++ Book_2/Chapter31/multiple_inheritance/listinherited2.py
@@ -0,0 +1,47 @@
+#!python
+
+
+class ListInherited:
+    """
+    Применяет dir() для сбора атрибутов экземпляра и имен, унаследованных
+    из его классов;
+    в Python 3.X отображается больше имен, чем в Python 2.X из-за наличия
+    подразумеваемого суперкласса object в модели классов нового стиля;
+    getattr() извлекает унаследованные имена не в self.__dict__;
+    используйте __str__, а не __repr__, иначе произойдет зацикливание при
+    вызове связанных методов!
+    """
+
+    def __attrnames(self):
+        result = ""
+        for attr in dir(self):
+            if attr[:2] == "__" and attr[-2:] == "__":
+                result += "\t%s\n" % attr
+            else:
+                result += "\t%s=%s\n" % (attr, getattr(self, attr))
+        return result
+    
+    def __attrnames(self, indent=" " * 4):
+        result = "Unders%s\n%s%%s\nOthers%s\n" % ("-" * 77, indent, "-" * 77)
+        unders = []
+        for attr in dir(self):
+            if attr[:2] == "__" and attr[-2:] == "__":
+                unders.append(attr)
+            else:
+                display = str(getattr(self, attr))[: 82 - (len(indent) + len(attr))]
+                result += "%s%s=%s\n" % (indent, attr, display)
+        return result % ", ".join(unders)
+        
+
+    def __str__(self):
+        return "<Instance of %s, address %s:\n%s>" % (
+            self.__class__.__name__,
+            id(self),
+            self.__attrnames(),
+        )
+
+
+if __name__ == "__main__":
+    import testmixin
+
+    testmixin.tester(ListInherited)
