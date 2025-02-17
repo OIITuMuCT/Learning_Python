bc8adb875a5798a754b62e3880861172630145d2
OIITuMuCT <vataman@live.ru>
2025-02-17 19:31:10 +0300

    Отображение сообщений об ошибках и трассировок

---
+++ Book_2/Chapter36/badly.py
@@ -0,0 +1,10 @@
+""" Отображение сообщений об ошибках и трассировок"""
+import traceback
+
+def inverse(x):
+    return 1/ x
+try:
+    inverse(0)
+except Exception:
+    traceback.print_exc(file=open('badly.exc', 'w'))
+print('Bye')
