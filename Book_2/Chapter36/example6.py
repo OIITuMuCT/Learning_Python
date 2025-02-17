522668ef70c76b10fe2800f56bf5480b18d59cd6
OIITuMuCT <vataman@live.ru>
2025-02-17 19:20:31 +0300

    Закрытие файлов и серверных подключений

--- Book_2/Chapter36/example6.py
+++ Book_2/Chapter36/example6.py
@@ -1,1 +1,17 @@
-myfile
+""" Закрытие файлов и серверных подключений """
+
+myfile = open('path/filename.txt', 'w')
+try:
+    ...обработка myfile...
+finally:
+    myfile.close()
+
+
+# Традиционная форма
+myfile = open('filename', 'w')
+
+myfile.close()
+
+# Форма с диспетчером контекста
+with open('filename') as myfile:
+    обработка myfile
