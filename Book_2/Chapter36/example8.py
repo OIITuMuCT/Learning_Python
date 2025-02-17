fcc9495094a2af9aa8563f3174be8e2e107a2ba3
OIITuMuCT <vataman@live.ru>
2025-02-17 19:27:43 +0300

    Выполнение внутрипроцессорных тестов

---
+++ Book_2/Chapter36/example8.py
@@ -0,0 +1,13 @@
+""" Выполнение внутрипроцессорных тестов """
+import sys
+log = open('testlog', 'a')
+from testapi import moreTests, runNextTest, testName
+def testdriver():
+    while moreTests():
+        try:
+            runNextTest()
+        except:
+            print('FAILED', testName(), sys.exc_info()[:2], file=log)
+        else:
+            print('PASSED', testName(), file=log)
+testdriver()
