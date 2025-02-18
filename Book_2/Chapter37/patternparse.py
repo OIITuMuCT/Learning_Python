9794f5c216a35c6e66e9a49c7dea8a5115b139d5
OIITuMuCT <vataman@live.ru>
2025-02-18 17:59:47 +0300

    domparse not working

--- Book_2/Chapter37/patternparse.py
+++ Book_2/Chapter37/patternparse.py
@@ -1,4 +1,5 @@
 import re
-text = open('mybooks.xml').read()
+text = open('mybooks.xml', 'r').read()
 found = re.findall('<title>(.*)</title>', text)
-for title in found: print(title)
+for title in found:
+    print(title)
