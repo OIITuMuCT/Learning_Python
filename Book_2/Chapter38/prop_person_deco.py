20a1101fee804d0d034126bad409138ba609dd51
OIITuMuCT <vataman@live.ru>
2025-02-18 20:31:22 +0300

    prop_person_deco

--- Book_2/Chapter38/prop_person_deco.py
+++ Book_2/Chapter38/prop_person_deco.py
@@ -3,11 +3,28 @@ class Person:
         self._name = name
     
     @property
-    def name(self):
-        "name property docs"
-        print('fetch...')
+    def name(self):             # name = property(name)
+        "name property docs"    # Документация по свойству name
+        print('fetch...')       # Извлечение...
         return self._name
     @name.setter
-    def name(self, name):
-        print('change...')
+    def name(self, value):      # name = name.setter(name)
+        print('change...')      # Изменение...
         self._name = value
+    @name.deleter               # name = name.deleter(name)
+    def name(self):
+        print('remove...')      # удаление...
+        del self._name
+
+if __name__ == '__main__':
+    bob = Person('Bob Smith')       # экземпляр bob имеет управляемый атрибут
+    print(bob.name)                 # Запускается метод getter свойства name (name1)
+    bob.name = 'Robert Smith'       # Запускается метод setter свойства name (name2) 
+    print(bob.name)
+    del bob.name                    # Запускается метод deleter свойства name (name3) 
+    
+    print('-'*20)
+    sue = Person('Sue Jones')       # Экземпляр sue тоже наследует свойство
+    print(sue.name)
+    print(Person.name.__doc__)      # Или help(Person.name)
+    # print(help(Person.name))
