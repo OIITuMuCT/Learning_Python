504e31d140bb59ab21b2c9a84bb4f0868c8e0196
OIITuMuCT <vataman@live.ru>
2025-02-19 14:29:48 +0300

    getattr_person

---
+++ Book_2/Chapter38/getattr_setattr/getattr_person.py
@@ -0,0 +1,30 @@
+class Person:
+    def __init__(self, name):
+        self._name = name
+    def __getattr__(self, attr):
+        print('get: ' + attr)
+        if attr == 'name':
+            return self._name
+        else:
+            raise AttributeError(attr)
+    def __setattr__(self, attr, value):
+        print('set: ' + attr)
+        if attr == 'name':
+            attr = '_name'
+        self.__dict__[attr] = value
+    def __delattr__(self, attr):
+        print('def: ' + attr)
+        if attr == 'name':
+            attr = '_name'
+        del self.__dict__[attr]
+
+if __name__ == '__main__':
+    bob = Person('Bob Smith')
+    print(bob.name)
+    bob.name = 'Robert Smith'
+    print(bob.name)
+    del bob.name
+    print('-'*20)
+    sue = Person('Sue Jones')
+    print(sue.name)
+    # print(Person.name.__doc__)
--- Book_2/Chapter38/getattr_setattr/wrapper_class.py
+++ Book_2/Chapter38/getattr_setattr/wrapper_class.py
@@ -1,16 +1,18 @@
+"""Отслеживание извлечения каждого атрибута, 
+сделанное другим объектом, который передается
+классу оболочки"""
 class Wrapper:
+    """ Перехват аттрибутов """
     def __init__(self, obj):
-        self.wrapped = obj
+        self.wrapped = obj                              # Сохранить объект
         self.name = 'Wrapper'
 
     def __getattr__(self, attrname):
-        print('Trace: ' + attrname)
-        return getattr(self.wrapped, attrname)
+        print("Trace: " + attrname)                     # Отслеживать извлечение
+        return getattr(self.wrapped, attrname)          # Делегировать извлечение
 
 
 if __name__ == '__main__':
     X = Wrapper([1, 2, 3])
-    X.append(4)
-    print(X.wrapped)
-    print(X.__dict__)
-
+    X.append(4)                                         # Выводится Trace: append
+    print(X.wrapped)                                    # Выводится [1, 2, 3, 4]
