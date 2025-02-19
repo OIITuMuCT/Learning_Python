7fc50f812a19a6581ff2acfdbd36c0630363dc51
OIITuMuCT <vataman@live.ru>
2025-02-19 11:00:00 +0300

    desc_both

---
+++ Book_2/Chapter38/descriptors/desc_both.py
@@ -0,0 +1,24 @@
+class DescBoth:
+    def __init__(self, data):
+        self.data = data
+    def __get__(self, instance, owner):
+        return '%s, %s' % (self.data , instance.data)
+    def __set__(self, instance, value):
+        instance.data = value
+
+class Client:
+    def __init__(self, data):
+        self.data = data
+    managed = DescBoth('spam')
+
+if __name__ == '__main__':
+    I = Client('eggs')
+    print(I.managed)
+    I.managed = "SPAM"
+    print(I.managed)
+    print(I.__dict__)
+    print([x for x in dir(I) if not x.startswith('__') ])
+    print(getattr(I, 'data'))
+    print(getattr(I, 'managed'))
+    for attr in (x for x in dir(I) if not x.startswith('__')):
+        print('%s => %s' % (attr, getattr(I, attr)))
