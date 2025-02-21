f6546da2ad17e9b3e7f57f09cfe3013980e139e3
OIITuMuCT <vataman@live.ru>
2025-02-21 22:16:42 +0300

    first_example

--- Book_1/Chapter16/example.py
+++ Book_1/Chapter16/example.py
@@ -12,3 +12,12 @@ else:
 
 othername = func  # Присваивание объекта функции
 othername()  # Снова вызывает func
+
+func.attr = 'spam'
+print(func.attr)
+print(func.__dict__)
+func.name = ''
+print(func.__dict__)
+func.name = print(f'{func.__name__} test positive or negative number')
+func.name
+print(func.__dict__)
---
+++ Book_1/Chapter16/first_example.py
@@ -0,0 +1,9 @@
+def times(x, y):
+    return x * y
+
+print(times(2, 4))
+x = times(3.14, 4)
+print(x)
+print(times('Ni', 4))
+times.pi = 3.14
+print(times(times.pi, 4))
