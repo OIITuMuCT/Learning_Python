42bc5b4f5f67890e30923ae912692bfa40ec8e6b
OIITuMuCT <vataman@live.ru>
2025-02-27 19:07:31 +0300

    metaclass4-super

---
+++ Book_2/Chapter40/meta/metaclass-super.py
@@ -0,0 +1,53 @@
+# Экземпляр нормально наследую метод перехвата вызов из классов 
+# и их суперклассов
+
+class SuperMetaObj:
+    def __call__(self, classname, supers, classdict):
+        print('In SuperMetaObj.call: ', classname, supers, classdict, sep='\n...')
+        Class = self.__New__(classname, supers, classdict)
+        self.__Init__(Class, classname, supers, classdict)
+        return Class
+
+class SubMetaObj(SuperMetaObj):
+    def __New__(self, classname, supers, classdict):
+        print('In SubMetaObj.new: ', classname, supers, classdict)
+        return type(classname, supers, classdict)
+    def __Init__(self, Class, supers, classname, classdict):
+        print('In SubMetaObj.init: ', classname, supers, classdict, sep='\n...')
+        print('...init class object:', list(Class.__dict__.keys()))
+
+
+
+class MetaObj:
+    def __call__(self, classname, supers, classdict):
+        print("In MetaObj.call: ", classname, supers, classdict, sep="\n...")
+        Class = self.__New__(classname, supers, classdict)
+        self.__Init__(Class, classname, supers, classdict)
+        return Class
+
+    def __New__(self, classname, supers, classdict):
+        print("In MetaObj.new: ", classname, supers, classdict)
+        return type(classname, supers, classdict)
+
+    def __Init__(self, Class, classname, supers, classdict):
+        print("In MetaObj.init: ", classname, supers, classdict, sep="\n...")
+        print("...init class object:", list(Class.__dict__.keys()))
+
+
+class Eggs:
+    pass
+
+
+print("making class")
+
+
+class Spam(Eggs, metaclass=SubMetaObj()):  # MetaObj - экземпляр нормального класса
+    data = 1  # вызывается в конце оператора class
+
+    def meth(self, arg):
+        return self.data + arg
+
+
+print("making instance")
+X = Spam()
+print("data: ", X.data, X.meth(2))
